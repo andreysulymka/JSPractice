f402c185386667eada00c2b3b012f5e1fa420b39
andreysulymka <andreysulymka5@gmail.com>
2024-07-30 16:07:20 +0300

    Initial commit

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+import './slider'
+import modals from "./modules/modals";
+import tabs from "./modules/tabs";
+
+window.addEventListener('DOMContentLoaded', () => {
+    modals();
+    tabs(
+        '.decoration_slider',
+        '.no_click',
+        '.decoration_content > div > div',
+        'after_click'
+    );
+})
